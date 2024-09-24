53517833bb0c4937c14afd524546e7d0802ba6e0
machichima <nary12321@gmail.com>
2024-09-24 22:09:59 +0800

    feat: read json if exist or crawl the info again

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	tools "github.com/machichima/taiwan-stock/internal"
 )
 
 func main() {
-    stockIDs, err := tools.GetAllStockID()
-    if err != nil {
-        panic(err)
+
+	stockInfoList := []tools.StockInfo{}
+
+    // Read from json file if exists
+    // else crawl the data
+	reader, err := os.Open("stockInfo.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+            
+            // crawl for data
+			stockInfoList, err = tools.GetAllStockInfoMonth("9")
+			if err != nil {
+				panic(err)
+			}
+
+		} else {
+            panic(err)
+        }
+	} else {
+        // read from the json file
+        if err := json.NewDecoder(reader).Decode(&stockInfoList); err != nil {
+            panic(err)
+        }
     }
 
-    fmt.Println(stockIDs)
+    fmt.Println(stockInfoList)
+
 }
